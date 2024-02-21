33e2a2971348f5e342ac39c0b31f2eb80c908d64
Ayomitunde Fafore <ayomitunde.fafore@snowflake.com>
2024-02-21 14:12:25 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,14 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
 
 
 def main():
     st.title("Welcome to Streamlit!")
 
     st.write("This is a simple Streamlit app that welcomes you.")
+    df = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+    st.line_chart(df)
 
     name = st.text_input("What's your name?")
     if name:
